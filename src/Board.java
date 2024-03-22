4453a10f1a866d1a9cb6251f3aa1f35bc63982a5
BryanCrom <BryanCrombach@gmail.com>
2024-03-22 17:18:36 +1300

    feat(board): create functionality for the board class

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,25 @@
 public class Board{
+    Spot[][] board;
 
+    public Board(){
+        this.clearBoard();
+    }
+
+    public void clearBoard(){
+        for(int row = 0; row < 3; row++){
+            for(int col = 0; col < 3; col++){
+                board[row][col] = new Spot(row, col, ' ');
+            }
+        }
+    }
+
+    public Spot getSpot(int row, int col) {
+        try{
+            return board[row][col];
+        }
+        catch(IndexOutOfBoundsException e){
+            System.out.println("This position does not exist on the board");
+        }
+        return null;
+    }
 }
